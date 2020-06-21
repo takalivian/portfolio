f57a7a55e02a270931d368be07808a405b7cbf91
takalivian <61145524+takalivian@users.noreply.github.com>
2020-06-21 20:39:17 +0900

    画像、名前表示

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Sample from './sample';
 import './App.css';
+ 
+class Name extends Component {
+  render() {
+    return <div>
+      <h1>Yoshifumi Takamura</h1>
+    </div>;
+  }
+}
 
 function App() {
+  const style = {
+    writingMode: "vertical-rl"
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={style}>
+        <Sample />
+        <Name />
+      </div>
     </div>
   );
 }
-
+ 
 export default App;
