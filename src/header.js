a6e228a8c5c076a9aae04de0903f2f0ba9fa522e
takalivian <61145524+takalivian@users.noreply.github.com>
2020-06-23 17:58:19 +0900

    スクロール自動

--- src/header.js
+++ src/header.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import ReactDOM from 'react-dom'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 class List extends Component {
+  style = {
+    color: "#000",
+    textDecoration: "none"
+  };
   render() {
     return <div>
       <ul>
-        <li id="list1">about</li>
-        <li id="list2">skill</li>
-        <li>work</li>
-        <li>contact</li>
+        <li class="active"><AnchorLink href='#about' style={this.style}>about</AnchorLink></li>
+        <li class="active"><AnchorLink href='#skill' style={this.style}>skill</AnchorLink></li>
+        <li class="active"><AnchorLink href='#work' style={this.style}>work</AnchorLink></li>
+        <li class="active"><AnchorLink href='#contact' style={this.style}>contact</AnchorLink></li>
       </ul>
     </div>;
   }
