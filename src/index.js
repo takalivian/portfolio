ff02d3c84fc5711baaf931f6b59f06493dcf5b6c
takalivian <61145524+takalivian@users.noreply.github.com>
2020-06-23 13:57:04 +0900

    fontawesome

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ import Skill from './skill';
 import Work from './work';
 import Contact from './contact';
 import * as serviceWorker from './serviceWorker';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(fab, fas, far);
 
 ReactDOM.render(
   <React.StrictMode>
