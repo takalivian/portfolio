bd0fe810c9bd8ff4d2210d47c326b012612ca5b6
takalivian <61145524+takalivian@users.noreply.github.com>
2020-06-23 22:01:50 +0900

    デプロイ

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB-3BQyonYadU7hMFx2UeiHsBmUhE8KB8c",
+  authDomain: "portfolio-a2f97.firebaseapp.com",
+  databaseURL: "https://portfolio-a2f97.firebaseio.com",
+  projectId: "portfolio-a2f97",
+  storageBucket: "portfolio-a2f97.appspot.com",
+  messagingSenderId: "1033177971936",
+  appId: "1:1033177971936:web:edbd306265d4656ee96485",
+  measurementId: "G-4TGVQ4X0PF"
+};
